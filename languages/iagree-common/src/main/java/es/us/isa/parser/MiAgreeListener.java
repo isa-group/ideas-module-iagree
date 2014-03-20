c93cdab817689bed70338412b6f2124b4b6a684d
Juanlu de la Fuente <juanlu.delafuente7@gmail.com>
2014-03-20 10:07:31 +0100

    Listeners actualizados

---
+++ languages/iagree-common/src/main/java/es/us/isa/parser/MiAgreeListener.java
@@ -0,0 +1,13 @@
+package es.us.isa.parser;
+
+import es.us.isa.parser.iAgreeParser.EntryContext;
+
+public class MiAgreeListener extends iAgreeBaseListener {
+	
+	@Override
+	public void enterEntry(EntryContext ctx) {
+		// TODO Auto-generated method stub
+		super.enterEntry(ctx);
+	}
+
+}
