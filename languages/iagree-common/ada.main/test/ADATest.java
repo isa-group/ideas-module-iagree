2f9a7deadd014dfd8d0d447089ecfe3adb11b6de
Mike <migsilvaz1@gmail.com>
2014-03-20 13:49:48 +0100

    Creado el test. Problema con el path de la métrica.

--- languages/iagree-common/ada.main/test/ADATest.java
+++ languages/iagree-common/ada.main/test/ADATest.java
@@ -1,5 +1,42 @@
 package test;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import es.us.isa.ada.ADA;
+import es.us.isa.ada.loaders.ExtensionsLoader;
+import es.us.isa.ada.loaders.ReflexionExtensionsLoader;
+import es.us.isa.ada.service.ADAServiceImpl;
+import es.us.isa.ada.service.ADAServiceV2;
+
 public class ADATest {
+	
+	public static void main(String[] args) throws Exception {
+		FileInputStream fileInputStream=null;
+        File file = new File("C:\\prueba.txt");
+        byte[] wsagBytes = new byte[(int) file.length()];
+        fileInputStream = new FileInputStream(file);
+	    fileInputStream.read(wsagBytes);
+	    fileInputStream.close();
+	    
+	    for (int i = 0; i < wsagBytes.length; i++) {
+	       	System.out.print((char)wsagBytes[i]);
+            }
+	    
+		ExtensionsLoader extLoad = new ReflexionExtensionsLoader();
+		ADA a = new ADA(extLoad);
+		ADAServiceV2 ada = new ADAServiceImpl(a);
+		Boolean isConsistent = false;
+		
+		if (ada.checkDocumentConsistency(wsagBytes)){	
+			if (ada.getDeadTerms(wsagBytes).size()==0){
+				if (ada.getLudicrousTerms(wsagBytes).size()==0){
+					isConsistent = true;
+				}
+			}
+		}
+		System.out.println(isConsistent);
+
+	}
 
 }
