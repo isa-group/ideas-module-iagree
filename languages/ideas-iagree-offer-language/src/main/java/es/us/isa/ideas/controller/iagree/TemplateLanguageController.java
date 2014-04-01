137df9a0a505df01a57e0f80c723d4b2e9d84470
Juanlu de la Fuente <juanlu.delafuente7@gmail.com>
2014-04-01 11:33:31 +0200

    Actualizado controlador

--- languages/ideas-iagree-offer-language/src/main/java/es/us/isa/ideas/controller/iagree/TemplateLanguageController.java
+++ languages/ideas-iagree-offer-language/src/main/java/es/us/isa/ideas/controller/iagree/TemplateLanguageController.java
@@ -1,24 +1,11 @@
 package es.us.isa.ideas.controller.iagree;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.us.isa.ideas.common.AppResponse;
 import es.us.isa.ideas.module.controller.BaseLanguageController;
-import es.us.isa.parser.MiAgreeListener;
-import es.us.isa.parser.iAgreeLexer;
-import es.us.isa.parser.iAgreeParser;
-import es.us.isa.parser.iAgreeParser.EntryContext;
+import es.us.isa.util.Conversion;
 
 @Controller
 @RequestMapping("/language")
@@ -84,37 +71,10 @@ public class TemplateLanguageController extends BaseLanguageController {
 		
 		if (currentFormat.equals("iagree") && desiredFormat.equals("wsag")) {
 			
-			String wsag = Conversion.
+			String wsag = Conversion.getWSAG(code);
 			//AppResponse resp = new AppResponse();
-
-			// AppAnnotations[] ann =
-			// ann.set
 		}
 
 		return null;
-	}
-
-	private static String loadSample(String filePath) {
-		// Location of file to read
-		File f = new File(filePath);
-		FileInputStream is;
-		String res = "";
-		try {
-			is = new FileInputStream(f);
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(is));
-			StringBuilder sb = new StringBuilder();
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				sb.append(line);
-			}
-			res = sb.toString();
-			is.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return res;
 	}
 }
