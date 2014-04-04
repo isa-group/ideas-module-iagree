3ae4ec179507bef005025beca8aa7fdef54c75be
Mike <migsilvaz1@gmail.com>
2014-04-04 13:50:22 +0200

    Compilación y configuración correcta, pero falla parseo de json

--- languages/ideas-iagree-template-language/src/main/java/es/us/isa/ideas/controller/iagree/TemplateLanguageController.java
+++ languages/ideas-iagree-template-language/src/main/java/es/us/isa/ideas/controller/iagree/TemplateLanguageController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import es.us.ideas.iagree.AnalizeDelegate;
 import es.us.isa.error.IAgreeError;
@@ -18,6 +19,12 @@ import es.us.isa.util.Convert2Wsag;
 @Controller
 @RequestMapping("/language")
 public class TemplateLanguageController extends BaseLanguageController {
+	
+	@RequestMapping("/pepe")
+	@ResponseBody
+	public String test() {
+		return "pepepepe";
+	}
 
 	public AppResponse executeOperation(String id, String content,
 			String fileUri, Map<String,String> data) {
