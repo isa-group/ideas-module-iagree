33d4bd865b151e82bed70458cf3e3131555704b6
muller <cmullercejas@gmail.com>
2014-05-22 17:29:16 +0200

    He añadido la comprobación de isLessRestrictiveOffer y también la de isMoreRestrictivaTemplateTermsThanCC al hacer la comprobación de conformidad.
    Esto se hacía en iAgreeStudio en el método POST del motón de compliance.
    Pero en IDEAS se había obviado y sólo se comprobaba la compliance simple
    entre documentos.

--- languages/iagree-common/src/main/java/es/us/isa/ideas/adaintegration/iagree/AnalizeDelegate.java
+++ languages/iagree-common/src/main/java/es/us/isa/ideas/adaintegration/iagree/AnalizeDelegate.java
@@ -7,7 +7,7 @@ import es.us.isa.ideas.common.AppResponse.Status;
 
 
 
-public class AnalizeDelegate {
+public class AnalizeDelegate { 
 	
 	public static AppResponse analize(String id, Map<String, Object> wsagAggregation,
 			Map<String, Object> wsagAggregationForComparation, boolean isOffer){
@@ -26,7 +26,7 @@ public class AnalizeDelegate {
 			try {
 				Boolean check = service.consistency(document);
 				if (check) {
-					appResponse.setMessage("The document is consistent.");
+					appResponse.setMessage("<pre>The document is consistent.</pre>");
 					appResponse.setStatus(Status.OK);
 				} else {
 					String inconsistencies = service.inconsitencyExplaining(document);
@@ -35,25 +35,25 @@ public class AnalizeDelegate {
 				}
 			} catch (Exception e) {
 				appResponse.setStatus(Status.ERROR);
-				appResponse.setMessage("There has been an error: "
-						+ e.getMessage());
+				appResponse.setMessage("<pre>There has been an error: "
+						+ e.getMessage() + "</pre>");
 			}
 		} else if (id.equals("getNumberOfAlternatives")) {
 			try {
 				Integer numAlt = service.getNumberOfAlternatives(document);
 				if (numAlt == 0) {
-					appResponse.setMessage("There are no alternatives for the document");
+					appResponse.setMessage("<pre>There are no alternatives for the document</pre>");
 				} else {
 					if (numAlt == 1) {
-						appResponse.setMessage("There is " + numAlt + " alternative to the document");
+						appResponse.setMessage("<pre>There is " + numAlt + " alternative to the document</pre>");
 					} else {
-						appResponse.setMessage("There are " + numAlt + " alternatives to the document");
+						appResponse.setMessage("<pre>There are " + numAlt + " alternatives to the document</pre>");
 					}
 				}
 				appResponse.setStatus(Status.OK);
 			} catch (Exception e) {
 				appResponse.setStatus(Status.ERROR);
-				appResponse.setMessage("There has been an error: " + e.getMessage());
+				appResponse.setMessage("<pre>There has been an error: " + e.getMessage() + "</pre>");
 			}
 		} else if (id.equals("checkCompliance") && isOffer) {
 			
@@ -62,23 +62,41 @@ public class AnalizeDelegate {
 				service.getService().addMetricFile(((String) wsagAggregationForComparation.get("metrics")).getBytes(), ((String) wsagAggregationForComparation.get("metricUri")).getBytes());
 				String otherDoc = (String) wsagAggregationForComparation.get("data");
 				
-				Boolean compliance = service.isCompliant(otherDoc, document);
+				Boolean compliance = false;
+				
+				Boolean isCompliant = service.isCompliant(otherDoc, document);
+				Boolean isLessRestrictiveOffer = service.isLessRestrictiveOffer(otherDoc, document);
+				Boolean isMoreRestrictiveTemplateTermsThanCC = service.isMoreRestrictiveTemplateTermsThanCC(otherDoc);
+							
+				
+				if (isCompliant){
+					//System.out.println("OK, Plantilla y oferta son compliant");
+					if (isLessRestrictiveOffer){
+						//System.out.println("OK, La oferta es menos o igual de restrictiva que la plantilla");
+						if (isMoreRestrictiveTemplateTermsThanCC){
+							//System.out.println("OK, Los términos de la plantilla son más o igual de restrictivos que las CCs de la plantilla");
+							compliance = true;
+						}
+					}	
+				}
+				
 				if (compliance) {
 					appResponse.setStatus(Status.OK);
-					appResponse.setMessage("The provided offer is compliant with the provided template");
+					appResponse.setMessage("<pre>The provided offer is compliant with the provided template</pre>");
 				} else {
 					String explanation = service.nonComplianceExplaining(
 							otherDoc, document);
 					appResponse.setStatus(Status.OK_PROBLEMS);
+					//appResponse.setMessage("<pre>" + explanation + "</pre>");
 					appResponse.setMessage(explanation);
 				}
 			} catch (Exception e) {
 				appResponse.setStatus(Status.ERROR);
-				appResponse.setMessage("There has been an error: " + e.getMessage());
+				appResponse.setMessage("<pre>There has been an error: " + e.getMessage() +"</pre>");
 			}
 		} else {
 			appResponse.setStatus(Status.ERROR);
-			appResponse.setMessage("No such opperation");
+			appResponse.setMessage("<pre>No such opperation</pre>");
 		}
 
 		return appResponse;
