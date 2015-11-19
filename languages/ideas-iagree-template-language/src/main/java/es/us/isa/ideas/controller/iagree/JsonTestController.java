46f45ee467448f5ed30abc0eb40fe05bda02f316
Juanlu <juanlu.delafuente7@gmail.com>
2015-11-19 13:53:51 +0100

    Test module implemented

--- languages/ideas-iagree-template-language/src/main/java/es/us/isa/ideas/controller/iagree/JsonTestController.java
+++ languages/ideas-iagree-template-language/src/main/java/es/us/isa/ideas/controller/iagree/JsonTestController.java
@@ -1,12 +1,10 @@
 package es.us.isa.ideas.controller.iagree;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.us.isa.ideas.module.controller.BaseTestModuleController;
 
 @Controller
-@RequestMapping("/tests")
 public class JsonTestController extends BaseTestModuleController {
 
 	@Override
